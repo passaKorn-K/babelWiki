717cf5835d519ce9c97fb7c64f7d23f3418be400
passaKorn-K <jakkajee380@gmail.com>
2017-05-22 13:47:15 +0700

    Add ui

---
+++ ui/components/App.js
@@ -0,0 +1,15 @@
+// App.js
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header>Navbar</header>
+        {this.props.children}
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ ui/index.js
@@ -0,0 +1,7 @@
+// index.js
+import React, {Component} from 'react'
+import {render} from 'react-dom'
+import routes from './routes'
+
+
+render(routes() ,document.getElementById('app'))
