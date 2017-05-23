fb77d995aeef509d64942fe3e0c37e27dd3eacd7
passaKorn-K <jakkajee380@gmail.com>
2017-05-23 20:22:25 +0700

    fix App

--- ui/components/App/App.js
+++ ui/components/App/App.js
@@ -2,21 +2,21 @@
 // กันอยู่ครับ ต่อไปนี้เราจะเอาไม่ทุกอย่างมาไว้ในไฟล์เดียวอีกแล้ว Header
 // เป็นตัวต่ออีกตัวที่เราต้องแยกออกไป แล้วนำมาประกบกับตัวหลักคือ App
 
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import Header from './Header'
 import styles from './App.scss'
 
 export default class App extends Component {
     render() {
-        return ( 
-        <div >
-            <Header / >
-                <div className = 'container' >
-                    <div className = { styles['content'] }> 
-                        { this.props.children }
-                    </div> 
-                </div> 
-        </div>
+        return (
+            <div >
+                <Header/>
+                <div className='container'>
+                    <div className={styles['content']}>
+                        {this.props.children}
+                    </div>
+                </div>
+            </div>
         )
     }
 }
