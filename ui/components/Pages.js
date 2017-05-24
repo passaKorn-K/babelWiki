88c7afd4b419fc7dd3dc6632fa8bb2f8254edeb6
passaKorn-K <jakkajee380@gmail.com>
2017-05-25 00:56:33 +0700

    Add state and componentDidMount

--- ui/components/Pages.js
+++ ui/components/Pages.js
@@ -1,8 +1,20 @@
 
 // Pages.js
 import React, { Component } from 'react'
+import fetch from 'isomorphic-fetch'
 
 class Pages extends Component {
+
+    state = {
+      pages: []
+    }
+
+    componentDidMount() {
+      fetch('http://127.0.0.1:5000/api/v1/pages')
+      .then((response) => response.json())
+      .then((pages) => this.setState({ pages }))
+    }
+    
   render() {
     return (
       <table className='table'>
